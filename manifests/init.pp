a23fb37e41d9a713999772a79d6bdea0c64857f0
Cliff Maury <cliff.maury@gmail.com>
2011-11-18 20:58:00 +0100

    Initial commit for submodule puppet-maven from Geppeto

---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+# Class: puppet-maven
+#
+# This module manages puppet-maven
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class puppet-maven {
+
+
+}
